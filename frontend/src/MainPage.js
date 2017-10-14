de5b5d8e0f9cd93f39015cc30634ba8da41bd211
yining <yining.chen@viacom.com>
2017-10-14 18:20:28 -0400

    add all posts' data to main page

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchCategories } from './actions'
+import {
+  fetchCategories,
+  fetchAllPosts
+} from './actions'
 
 class MainPage extends Component {
   componentDidMount () {
     this.props.dispatch(fetchCategories())
+    this.props.dispatch(fetchAllPosts())
   }
 
   render () {
-    const { categories } = this.props
+    const { categories, posts } = this.props
     return (
       <div className='main-page'>
+        <h1>Categories</h1>
         <ul>
           {categories.map(({ name, path }) => (
             <li key={name}>
               <a href={path}>{name}</a>
+            </li>
+          ))}
+        </ul>
+        <h1>Posts</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <a href={post.category + '/' + post.id}>
+                {post.title}
+              </a>
             </li>
           ))}
         </ul>
@@ -23,8 +38,8 @@ class MainPage extends Component {
   }
 }
 
-function mapStateToProps ({ categories }) {
-  return { categories }
+function mapStateToProps ({ categories, posts }) {
+  return { categories, posts }
 }
 
 export default connect(mapStateToProps)(MainPage)
